2702fd5896a3ea024353cc92236b41b01ef561f5
ludyhasby <fewesgalih@gmail.com>
2024-08-19 10:15:24 +0700

    permasalahan umum

--- pages/3_🧩_Latent Dirichlet Analysis.py
+++ pages/3_🧩_Latent Dirichlet Analysis.py
@@ -81,7 +81,18 @@ df1["sumber"] = "ps"
 df2 = pd.read_csv("ios_processing.csv")
 df2["sumber"] = "as"
 df = pd.concat([df1, df2])
-df["datetime_baru"] = pd.to_datetime(df["datetime_baru"]) + timedelta(hours=7)
+# tambahan
+def load_data(id, sheet_name)-> pd.DataFrame:
+  url = f'https://docs.google.com/spreadsheets/d/{id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
+  return pd.read_csv(url, parse_dates=[0, 1, 2, 3, 4], infer_datetime_format=True)
+id = "1oTSMV4_VAoZEvU7-bHJLoKsg-mSGpFhxyFQLSOjl3VI"
+sheet_name = "temp"
+df1_tambahan = load_data(id, sheet_name)
+df1_tambahan["datetime_baru"]= pd.to_datetime(df1_tambahan["datetime_baru"])
+df1_tambahan["score"] = df1_tambahan["score"].astype(int)
+df = pd.concat([df, df1_tambahan])
+
+df["datetime_baru"] = pd.to_datetime(df["datetime_baru"]) 
 df["tahun_bulan"] = df["datetime_baru"].dt.to_period('M')
 df["tahun_bulan_01"] = pd.to_datetime(df["tahun_bulan"].astype(str) + "-01")
 df["jam"] = df["datetime_baru"].dt.hour
@@ -116,6 +127,42 @@ def social_icons(width=24, height=24, **kwargs):
 
         return icons_html
 #####################
+def penampilan_gangguan(nama_gangguan, bulan_hari, jumlah_data):
+    if bulan_hari == "Bulanan":
+        range_time = 'tahun_bulan_01'
+    else:
+        range_time = 'tanggal'
+    if nama_gangguan == "Tidak Bisa Login":
+        data = df_tidak_buka
+        st.write("Note: Diterapkan jika timeout maka pasti tidak bisa login, dan tidak sebaliknya")
+    elif nama_gangguan == "Susah digunakan":
+        data = df_susah
+    elif nama_gangguan == "Gangguan Transfer":
+        data = df_transfer
+    elif nama_gangguan == "Gangguan Update":
+        data = df_update
+    elif nama_gangguan == "Gangguan Transaksi":
+        data = df_transaksi
+    elif nama_gangguan == "Kehabisan Waktu":
+        data = df_kehabisan_waktu
+    elif nama_gangguan == "Gangguan Deteksi Wajah":
+        data = df_wajah
+    elif nama_gangguan == "Lambat Saat Digunakan":
+        data = df_lambat
+    elif nama_gangguan == "Force Close":
+        data = df_fc
+    
+    # tampilkan time series nya 
+    ts = data.groupby([f'{range_time}']).size().reset_index()
+    ts.rename({f'{range_time}': 'Waktu Diskrit', 0:'Jumlah Ulasan'}, axis=1, inplace=True)
+    fig = px.line(ts, x ='Waktu Diskrit', y=["Jumlah Ulasan"], title=f'Perkembangan Gangguan {nama_gangguan} dalam {PERIOD}', 
+                    color_discrete_sequence=['#ff0000'])
+    fig.update_xaxes(tickangle=45)
+    st.plotly_chart(fig, theme="streamlit")
+    # tampilkan contohnya 
+    st.write(f"Contoh {jumlah_data} Data {nama_gangguan}")
+    st.dataframe(data.head(jumlah_data)["Sentences"].reset_index(drop=True))
+
 
 text1, pict1 = st.columns((4,2))
 with text1:
@@ -185,27 +232,25 @@ with st.sidebar:
             </script>
             """
             st.markdown(js, unsafe_allow_html=True)
-
-tab1, tab2 = st.tabs(["Eksplorasi LDA Result🌍", "Mengenal Latent Dirichlet Analysis🏫"])
+PERIOD = st.selectbox("Filter Berdasar Waktu", options=["All Times", "2024", "Minggu Terakhir", "Custom Date", "Pilih Bulan"], index=0)
+if PERIOD == "2024":
+    df = df[df["datetime_baru"].dt.year==2024]
+elif PERIOD == "Minggu Terakhir":
+    BatasMinggu = maksDate - timedelta(days=7)
+    df = df[(df["tanggal"]>=BatasMinggu) & (df["tanggal"]<=maksDate)]
+    PERIOD = str(BatasMinggu) + " - "+ str(maksDate)
+elif PERIOD == "Custom Date":
+    start_date, end_date = st.date_input("For Custome Date, Pick a Date Please", (minDate, maksDate),min_value=minDate, max_value=maksDate)
+    df = df[(df["tanggal"]>=start_date) & (df["tanggal"]<=end_date)]
+    PERIOD = str(start_date) + " - "+ str(end_date)
+elif PERIOD == "Pilih Bulan":
+    bulan = st.multiselect("Pilih Nomor Bulan", options= sorted(df["bulan"].unique()), default=sorted(df["bulan"].unique()))
+    st.caption("Notes: 1 = Januari, 12 = Desember, dst.")
+    df = df[df["bulan"].isin(bulan)]
+elif PERIOD == "All Times":
+    PERIOD = PERIOD + " (" + str(minDate) + " - "+ str(maksDate) + ")"
+tab1, tab2 = st.tabs(["Eksplorasi LDA Result🌍", "Permasalahan Umum pada BSI Mobile 🏫"])
 with tab1:
-    PERIOD = st.selectbox("Filter Berdasar Waktu", options=["All Times", "2024", "Minggu Terakhir", "Custom Date", "Pilih Bulan"], index=0)
-    if PERIOD == "2024":
-        df = df[df["datetime_baru"].dt.year==2024]
-    elif PERIOD == "Minggu Terakhir":
-        BatasMinggu = maksDate - timedelta(days=7)
-        df = df[(df["tanggal"]>=BatasMinggu) & (df["tanggal"]<=maksDate)]
-        PERIOD = str(BatasMinggu) + " - "+ str(maksDate)
-    elif PERIOD == "Custom Date":
-        start_date, end_date = st.date_input("For Custome Date, Pick a Date Please", (minDate, maksDate),min_value=minDate, max_value=maksDate)
-        df = df[(df["tanggal"]>=start_date) & (df["tanggal"]<=end_date)]
-        PERIOD = str(start_date) + " - "+ str(end_date)
-    elif PERIOD == "Pilih Bulan":
-        bulan = st.multiselect("Pilih Nomor Bulan", options= sorted(df["bulan"].unique()), default=sorted(df["bulan"].unique()))
-        st.caption("Notes: 1 = Januari, 12 = Desember, dst.")
-        df = df[df["bulan"].isin(bulan)]
-    elif PERIOD == "All Times":
-        PERIOD = PERIOD + " (" + str(minDate) + " - "+ str(maksDate) + ")"
-    
     st.header("Topik Yang Sering Muncul berdasar LDA")
     kol1, kol2 = st.columns(2)
     with kol1:
@@ -246,22 +291,71 @@ with tab1:
     with colc:
         background = st.color_picker("Background Warna", value="#C5F5F0")
 
-    # Fitur Lengkap dan Memudahkan Transaksi
-    detail_topik("Fitur Lengkap dan Memudahkan Transaksi", 6, background, lowerOrUpper, maks_kata)
-    # Kenyamanan pada BSI Mobile
-    detail_topik("Kenyamanan pada BSI Mobile", 2, background, lowerOrUpper, maks_kata)
-    # BSI Mobile Susah digunakan dan Sering Eror
-    detail_topik("BSI Mobile Susah digunakan dan Sering Eror", 3, background, lowerOrUpper, maks_kata)
-    # BSI Mobile Keren dan Lancar Digunakan
-    detail_topik("BSI Mobile Keren dan Lancar Digunakan", 1, background, lowerOrUpper, maks_kata)
-    # Permasalahan pada BSI Mobile (Kode Aktivasi, Rekening)
-    detail_topik("Permasalahan pada BSI Mobile (Kode Aktivasi, Rekening)", 5, background, lowerOrUpper, maks_kata)
-    # Transaksi Nasabah pada BSI Mobile
-    detail_topik("Transaksi Nasabah pada BSI Mobile", 0, background, lowerOrUpper, maks_kata)
-    # Aplikasi Berguna tapi Ribet
-    detail_topik("Aplikasi Berguna tapi Ribet", 4, background, lowerOrUpper, maks_kata)
+    # # Fitur Lengkap dan Memudahkan Transaksi
+    # detail_topik("Fitur Lengkap dan Memudahkan Transaksi", 6, background, lowerOrUpper, maks_kata)
+    # # Kenyamanan pada BSI Mobile
+    # detail_topik("Kenyamanan pada BSI Mobile", 2, background, lowerOrUpper, maks_kata)
+    # # BSI Mobile Susah digunakan dan Sering Eror
+    # detail_topik("BSI Mobile Susah digunakan dan Sering Eror", 3, background, lowerOrUpper, maks_kata)
+    # # BSI Mobile Keren dan Lancar Digunakan
+    # detail_topik("BSI Mobile Keren dan Lancar Digunakan", 1, background, lowerOrUpper, maks_kata)
+    # # Permasalahan pada BSI Mobile (Kode Aktivasi, Rekening)
+    # detail_topik("Permasalahan pada BSI Mobile (Kode Aktivasi, Rekening)", 5, background, lowerOrUpper, maks_kata)
+    # # Transaksi Nasabah pada BSI Mobile
+    # detail_topik("Transaksi Nasabah pada BSI Mobile", 0, background, lowerOrUpper, maks_kata)
+    # # Aplikasi Berguna tapi Ribet
+    # detail_topik("Aplikasi Berguna tapi Ribet", 4, background, lowerOrUpper, maks_kata)
 
 with tab2:
-    st.write("Apabila diperlukan.")
+    st.title("Permasalahan Umum pada BSI Mobile")
+    dict_gangguan={}
+    ## Permintaan Kehabisan Waktu
+    p_kw = r"\b(waktu|time|kehabisan)\w*"
+    df_kehabisan_waktu = df[(df["Preprocess_Sentences"].str.contains(p_kw, regex=True, na=False)) & (df["sentiment"]=="negative")]
+    dict_gangguan["Kehabisan Waktu"] = len(df_kehabisan_waktu)
+    ## Tidak bisa Login 
+    st.write("Catatan: Jika Timeout pasti tidak bisa login, namun tidak pasti sebaliknya")
+    p_tidak_buka = r"\b(buka|bisa buka|login|masuk|waktu|time|kehabisan)\w*"
+    df_tidak_buka = df[(df["Preprocess_Sentences"].str.contains(p_tidak_buka, regex=True, na=False)) & (df["sentiment"]=="negative")]
+    dict_gangguan["Tidak Bisa Login"] = len(df_tidak_buka)
+    ## Force Close 
+    p_fc = r"\b(force|close|tutup paksa)\w*"
+    df_fc = df[(df["Preprocess_Sentences"].str.contains(p_fc, regex=True, na=False)) & (df["sentiment"]=="negative")]
+    dict_gangguan["Force Close"] = len(df_fc)
+    ## Lambat, lemot
+    p_lambat = r'\b(lambat|lemot|macet|lelet|pending|panding|lag|leg|bug)\w*'
+    df_lambat = df[df["Preprocess_Sentences"].str.contains(p_lambat, regex=True, na=False) & (df["sentiment"] == "negative")]
+    dict_gangguan["Lambat Saat Digunakan"] = len(df_lambat)
+    ## Gangguan Deteksi Wajah, Verifikasi, Senyum
+    p_wajah = r'\b(wajah|deteksi|verifikasi|senyum)\w*'
+    df_wajah = df[df["Preprocess_Sentences"].str.contains(p_wajah, regex=True, na=False) & (df["sentiment"] == "negative")]
+    dict_gangguan["Gangguan Deteksi Wajah"] = len(df_wajah)
+    ## Sulit, Susah, Ribet
+    p_susah = r"\b(sulit|susah|ribet)\w*"
+    df_susah = df[df["Preprocess_Sentences"].str.contains(p_susah, regex=True, na=False) & (df["sentiment"]=="negative")]
+    dict_gangguan["Susah digunakan"] = len(df_susah)
+    ## gangguan transfer
+    df_transfer = df[(df["Preprocess_Sentences"].str.contains(r"\btransfer\w*", regex=True, na=False)) & (df["sentiment"]=="negative")]
+    dict_gangguan["Gangguan Transfer"] = len(df_transfer)
+    ## gangguan Transaksi
+    df_transaksi = df[df["Preprocess_Sentences"].str.contains(r"\btransaksi\w*", regex=True, na=False) & (df["sentiment"]=="negative")]
+    dict_gangguan["Gangguan Transaksi"] = len(df_transaksi)
+    ## Gangguan update, upgrade 
+    p_update = r"\b(update|upgrade|pembaruan|upd)\w*"
+    df_update = df[(df["Preprocess_Sentences"].str.contains(p_update, regex=True, na=False)) & (df["sentiment"]=="negative")]
+    dict_gangguan["Gangguan Update"] = len(df_update)
+
+    df_gangguan = pd.DataFrame(list(dict_gangguan.items()), columns=['Jenis Gangguan', 'Jumlah Ulasan'])
+    st.dataframe(df_gangguan.sort_values("Jumlah Ulasan", ascending=False).reset_index(drop=True))
+    k1, k2, k3 = st.columns(3)
+    with k1:
+        jenis_gangguan = st.selectbox(label="Jenis Gangguan", options=df_gangguan['Jenis Gangguan'].unique(), index=0)
+    with k2:
+        rentang_waktu = st.selectbox(label="Rentang Waktu", options=["Bulanan", "Harian"], index=0)
+    with k3:
+        banyak_contoh_data = st.number_input("Banyak contoh data untuk ditampilkan", value=15)
+    penampilan_gangguan("Tidak Bisa Login", rentang_waktu, banyak_contoh_data)
+
+
 
 st.markdown("*Copyright © 2024 Ludy Hasby Aulia*")
